dc2147464c1892eb8629f91667c4790cc740f262
Varun Mathur <varunmathur817@gmail.com>
2026-07-22 00:08:03 +0530

    abut

--- component/about/Section1.jsx
+++ component/about/Section1.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Section1() {
   return (
-    <section className="relative w-full h-[450px] md:h-[300px] lg:h-[400px] xl:h-[500px] overflow-hidden">
+    <section className="relative w-full h-[500px] md:h-[300px] lg:h-[400px] xl:h-[500px] overflow-hidden">
       {/* Banner Image */}
       <Image
         src="/about/aboutbanner.jpg"
